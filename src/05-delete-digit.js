289a32637f8b5c7a313d5a1fe1b64a1871685815
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-04-08 15:27:11 +0300

    feat: implement task 05

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,15 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const res = [];
+  const digits = `${n}`.split('');
+  for (let i = 0; i < digits.length; i += 1) {
+    const newN = [...digits];
+    newN.splice(i, 1);
+    res.push(+newN.join(''));
+  }
+  return res.sort((a, b) => b - a)[0];
 }
 
 module.exports = deleteDigit;
