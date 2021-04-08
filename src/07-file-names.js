3c2acc9f4a24b1b614c5ea90443337ece32f4b9e
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-04-08 17:51:16 +0300

    feat: implement task 07

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,18 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+const addSuffix = (name, arr, k = 1) => {
+  const newName = `${name}(${k})`;
+  return arr.includes(newName) ? addSuffix(name, arr, k + 1) : newName;
+};
+
+function renameFiles(names) {
+  const res = [];
+  for (let i = 0; i < names.length; i += 1) {
+    const name = names[i];
+    res.push(res.includes(name) ? addSuffix(name, res) : name);
+  }
+  return res;
 }
 
 module.exports = renameFiles;
