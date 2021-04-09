fd6b87eceadf71d8ec26d9a60514d3ea952dcb83
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-04-09 17:54:07 +0300

    feat: implement task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let res = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i - 1] === str[i]) {
+      count += 1;
+    } if (str[i + 1] !== str[i]) {
+      res += `${count === 1 ? str[i] : count + str[i]}`;
+      count = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
